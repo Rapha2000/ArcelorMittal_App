85ba46163d599f4920c4655460957458930a7997
Rapha2000 <121192213+Rapha2000@users.noreply.github.com>
2023-02-27 16:46:14 +0100

    Update DaoManagerH2.java

--- controller/DaoManagerH2.java
+++ controller/DaoManagerH2.java
@@ -1,1 +1,37 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+public class DaoManagerH2 {
+	
+	private static DaoManagerH2 instance = null;
+	private Connection conn = null;
+	
+	private DaoManagerH2() {
+		try {
+			// Informations de connexion à la base de données H2
+		    String jdbcUrl = "jdbc:h2:&~/test";
+		    String username = "sa";
+		    String password = "sa";
+		    conn = DriverManager.getConnection(jdbcUrl, username, password);
+		} catch(Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Connection to H2 db succeeded");
+	
+	}
+
+	
+    public static DaoManagerH2 getInstance() {
+    	if (instance == null)
+			instance = new DaoManagerH2();
+		return instance;
+    }
+	
+}
+
 
