228018e8f2263890e66a5732e40b18683663aab2
anaisdespres9 <125544582+anaisdespres9@users.noreply.github.com>
2023-03-10 11:54:16 +0100

    Add files via upload

---
+++ data_types/Users.java
@@ -0,0 +1,17 @@
+package donnees;
+
+public class Users {
+
+	private int id ;
+	private String name;
+	
+	
+	public Users(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+	
+}
